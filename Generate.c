569bde0fe42f256171eb6b246aca31c90c88dfdf
Xun Lin <xunlin@bu.edu>
2019-11-01 10:58:18 -0400

    ad

---
+++ Generate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int generate_weight(){
+	
+
+
+}
+int main(int argc, char *argv[]){
+	int n=0,c=0;
+	for (int i = 1; i < argc; ++i) {
+		argv[]
+	}
+        
+}
