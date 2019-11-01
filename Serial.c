b87d9f737762e3ea0f3c99f18776b2eb65c496aa
Xun Lin <xunlin@bu.edu>
2019-11-01 11:42:20 -0400

    worker

--- Serial.c
+++ Serial.c
@@ -27,13 +27,36 @@ void LoadInput(char file[],
     fclose (infile); 
 }
 
+void Worker(int* weight, int* values, int* result, int n, int b){
+		int i, j;
+		for(j=0; j<b; j++){
+			if (weight[0] > j) {
+	  		result[0][j] = 0;
+				} 
+			else {
+	  			result[0][j] = values[0];
+			}
+		}
+		for(i = 1; i<n; i++){
+			for(j=0; j<b; j++){
+				if (j < weight[i] || result[i-1][j] >= result[i-1][j-weight[i]] + value[i])){
+					result[i][j]=result[i-1][j];
+				}
+				else{
+					result[i][j] = result[i-1][j-weight[i]] + value[i];
+				}
+			}
+		}
+}
 
 
 int main(int argc, char *argv[]){
 		int num_painting,num_bags;
-    int weights;
-    int values;
-		LoadInput("test1.txt",&num_painting, &num_bags, &weights, &values);
-
+    int *weights;
+    int *values;
+    int *result;
+		LoadInput("test1.txt",&num_painting, &num_bags, weights, values);
+		result = malloc(sizof(int[num_painting][num_bags]));
+		Worker(weights, values, result, num_painting, num_bags);
 		return 0; 
 }
