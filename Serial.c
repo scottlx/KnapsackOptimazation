709e957fb7b65da4fbcca0fb9e2a0f11b7a7d75d
JiaxinZhang <jiaxin@bu.edu>
2019-11-01 11:38:18 -0400

    load function done

--- Serial.c
+++ Serial.c
@@ -6,34 +6,57 @@ void LoadInput(char file[],
 							 int* weights, int* values){
 		FILE *infile; 
 		char filename[64];
-		int i;
+		int i, temp_int;
 		snprintf(filename, sizeof(filename), "%s", file);
    
     // Open input txt file with error check 
     infile = fopen (filename, "r"); 
-    if (infile == NULL) { 
+    if (infile == NULL) {
         fprintf(stderr, "\nError opening file\n"); 
         exit (1); 
     } 
     
     // Read integers
     
-  	fscanf (infile, "%d", &i);    
-  	while (!feof (infile)){  
-      printf ("%d ", i);
-      fscanf (infile, "%d", &i);      
-    }
-
+    // load number of painting in the first line
+  	fscanf (infile, "%d", &temp_int);
+  	*num_painting = temp_int;
+  	
+  	// load number of bags in the first line
+  	fscanf (infile, "%d", num_bags);
+  	
+  	
+  	// prepare memory for weights and values array
+  	weights = malloc((*num_painting)*sizeof(int));
+  	values = malloc((*num_painting)*sizeof(int));
+  	
+  	// load weights
+  	for(i=0; i<(*num_painting); i++){
+  		fscanf (infile, "%d", &temp_int);
+  		weights[i] = temp_int;
+  	}
+  	
+  	// load values
+  	for(i=0; i<(*num_painting); i++){
+  		fscanf (infile, "%d", &temp_int);
+  		values[i] = temp_int;
+  	}
     fclose (infile); 
+    
+    //printf("number of bags = %d\n", *num_bags);
+    //printf("number of paintings = %d\n", *num_painting);
 }
 
 
 
+
+
 int main(int argc, char *argv[]){
-		int num_painting,num_bags;
+		int num_painting, num_bags;
     int weights;
     int values;
-		LoadInput("test1.txt",&num_painting, &num_bags, &weights, &values);
-
+		LoadInput("test.txt", &num_painting, &num_bags, &weights, &values);
+		
+		
 		return 0; 
 }
