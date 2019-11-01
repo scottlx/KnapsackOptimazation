1f787fd2b69ac20bc2144c72cb5819d451c92c1e
JiaxinZhang <jiaxin@bu.edu>
2019-11-01 11:21:06 -0400

    load txt function

--- Serial.c
+++ Serial.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void LoadInput(char file[], 
+							 int* num_painting, int* num_bags,
+							 int* weights, int* values){
+		FILE *infile; 
+		char filename[64];
+		int i;
+		snprintf(filename, sizeof(filename), "%s", file);
+   
+    // Open input txt file with error check 
+    infile = fopen (filename, "r"); 
+    if (infile == NULL) { 
+        fprintf(stderr, "\nError opening file\n"); 
+        exit (1); 
+    } 
+    
+    // Read integers
+    
+  	fscanf (infile, "%d", &i);    
+  	while (!feof (infile)){  
+      printf ("%d ", i);
+      fscanf (infile, "%d", &i);      
+    }
+
+    fclose (infile); 
+}
+
+
 
 int main(int argc, char *argv[]){
+		int num_painting,num_bags;
+    int weights;
+    int values;
+		LoadInput("test1.txt",&num_painting, &num_bags, &weights, &values);
 
-	return 0; 
+		return 0; 
 }
